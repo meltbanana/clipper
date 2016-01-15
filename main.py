fba389ee59164842778adcc1206fbb2b09b3e4c4
meltbanana <meltbanana@yandex.ru>
2016-01-15 18:32:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,36 @@
 import numpy as np
 from boat import Boat
 
-
 class env:
     pass
-env.wind = np.array([1, 0])
+env.wind = np.array([10, 0])
 
 def control(boat):
-    boat.speed += 1
+ 
+    print(boat)
+    
+    sail = input('set sail, captain:')
+    try:
+        boat.sail = int(sail) * np.pi/ 180   
+    except:
+        if sail == 'exit':
+            print('\n\033[31m' + 'game over' + '\033[30m')
+            exit()
+    
+    wheel = input('set wheel, captain:')
+    try:
+        boat.wheel = int(wheel) * np.pi/ 180   
+    except:
+        pass
+    
     return True
 
 
 boat = Boat()
 
-for t in range(10):
-    control(boat)
+for t in range(1000):
+    if t%10 == 0: 
+        print('\n\033[31m' + '--- control session --- (print exit to exit)---'+'\033[30m')
+        print('wind = ', env.wind, ', apparent wind =', env.wind - boat.speed)
+        control(boat)
     boat.move(env)
-    print(boat.speed)
